8310d56085732ddfbaa43442b6c2ba2061d9d1f4
Paul Belanger <pabelanger@redhat.com>
2017-09-21 11:12:20 -0400

    Use ::fqdn for nodepoolv3 yaml file
    What we really want to the fqdn, since that is how we store the
    filename in project-config.
    
    Change-Id: Ie1337ce233bc7f270acfcdab7632326d412b05e5
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- manifests/init.pp
+++ manifests/init.pp
@@ -52,7 +52,7 @@ class project_config(
   $nodepool_scripts_dir           = "/etc/project-config/${base}nodepool/scripts"
   $nodepool_elements_dir          = "/etc/project-config/${base}nodepool/elements"
   $nodepool_config_file           = "/etc/project-config/${base}nodepool/nodepool.yaml"
-  $nodepool_config_file_zuulv3    = "/etc/project-config/${base}nodepool/${::hostname}.yaml"
+  $nodepool_config_file_zuulv3    = "/etc/project-config/${base}nodepool/${::fqdn}.yaml"
   $zuul_layout_dir                = "/etc/project-config/${base}zuul"
   $zuul_site_variables_yaml       = "/etc/project-config/${base}zuul/site-variables.yaml"
 }
